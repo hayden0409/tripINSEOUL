2c75b230334fd20688cb5f4e0f8494d664af3a0d
hayden <duddls04099@gmail.com>
2022-02-20 19:38:53 +0900

    초기 설정

---
+++ src/main/java/com/example/dao/MemberDAO.java
@@ -0,0 +1,13 @@
+package com.example.dao;
+
+import java.util.List;
+	 
+import com.example.dto.MemberVO;
+	 
+public interface MemberDAO {
+	    
+	    public List<MemberVO> selectMember() throws Exception;
+	}
+
+
+
